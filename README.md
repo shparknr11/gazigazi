5ef62339a835d1067957d4e73387e7b1e63c3f92
shparknr11 <shparknr11@naver.com>
2024-06-28 18:14:28 +0900

    docs : 라우터 및 퍼블리싱 구조 작업

--- README.md
+++ README.md
@@ -15,22 +15,23 @@
 - react-icon
 - react-calendar
 
+## 코딩컨벤션
+
+카멜케이스 projectStart
+파스칼케이스 ProjectStart
+스네이크케이스 project_start
+소문자 projectstart
+
+[ 폴더 생성시 소문자]
+[ 페이지컴포넌트 파스칼케이스]
+[ 컴포넌트 파스칼 케이스]
+[ public 폴더 내 파일 소문자]
+[api 파일 소문자]
+[path 경로 카멜케이스]
+[변수명 카멜케이스]
+[함수명 카멜케이스]
+[클레스명 스네이크케이스]
+[매개변수 _project]
+[이미지파일 123.png , project.png, start123.png]
+
 ## 라우터패스 설정
-
-- 로그인 페이지
-  : /login
-  : /login/idfind
-  : /login/pwfind
-
-- 홈페이지
-  : /
-
-- 모임 개설페이지
-  : /create
-- 모임 상세페이지
-  : /detail/:id
-- 모임 검색페이지
-  : /search
-- 모임 카테고리목록 페이지
-  : /category
-- 마이페이지
--- src/App.js
+++ src/App.js
@@ -1,6 +1,50 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 const App = () => {
-  return <>프로젝트 시작.</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<h1>로그인페이지</h1>}></Route>
+        <Route path="/createaccount" element={<h1>회원가입페이지</h1>}></Route>
+        <Route path="/findid" element={<h1>아이디찾기</h1>}></Route>
+        <Route path="/findpw" element={<h1>비밀번호찾기</h1>}></Route>
+
+        <Route path="/" element={<h1>홈 페이지</h1>}></Route>
+
+        <Route path="/creatmeeting" element={<h1>모임등록페이지</h1>}></Route>
+        <Route
+          path="/meeting/:meetingId"
+          element={<h1>모임상세페이지</h1>}
+        ></Route>
+        <Route path="/myprofile/:userId" element={<h1>마이페이지</h1>}></Route>
+        <Route
+          path="/interestlist/:userId"
+          element={<h1>찜목록페이지</h1>}
+        ></Route>
+        <Route path="/search" element={<h1>검색페이지</h1>}></Route>
+        <Route path="/category" element={<h1>카테고리 페이지</h1>}></Route>
+
+        <Route path="/notice" element={<h1>커뮤니티 페이지</h1>}></Route>
+        <Route
+          path="/notice/:boardId"
+          element={<h1>커뮤상세페이지</h1>}
+        ></Route>
+        <Route path="/notice/write" element={<h1>커뮤등록페이지</h1>}></Route>
+        <Route
+          path="/notice/modify/:boardId"
+          element={<h1>커뮤수정페이지</h1>}
+        ></Route>
+
+        <Route path="/mymeeting" element={<h1>내모임페이지 </h1>}></Route>
+        <Route
+          path="/mymeeting/:meetingId"
+          element={<h1>내모임상세페이지</h1>}
+        ></Route>
+
+        <Route path="*" element={<h1>404 잘못된 경로입니다.</h1>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
