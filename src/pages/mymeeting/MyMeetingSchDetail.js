e39aa0ceec719079c32aee787c10b39983753d54
shparknr11 <shparknr11@naver.com>
2024-07-19 18:08:17 +0900

    feat:  MyMeetingSchDetail 수정

--- src/pages/mymeeting/MyMeetingSchDetail.js
+++ src/pages/mymeeting/MyMeetingSchDetail.js
@@ -2,6 +2,7 @@ import styled from "@emotion/styled";
 import { useEffect, useState } from "react";
 import {
   deleteSchOne,
+  getSchMemberAll,
   getSchMemberSeq,
   getSchOne,
   patchSch,
@@ -108,6 +109,129 @@ const TitleDivStyle = styled.div`
   padding-left: 5px;
   padding-top: 20px;
 `;
+const ReactCalendarStyle = styled.div`
+  width: 35%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 20px;
+  .cut-text {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .item-wrap {
+    display: flex;
+    width: 20%;
+    justify-content: center;
+    align-items: center;
+    font-size: 18px;
+    font-weight: bold;
+    border: 1px solid #f7ebd5;
+    border-radius: 4px 4px 0px 0px;
+    flex-direction: column;
+    justify-content: start;
+  }
+  .member-wrap {
+    display: flex;
+    width: 100%;
+    text-align: center;
+  }
+  .item {
+    width: 100%;
+    padding: 10px;
+    color: #383737;
+    cursor: pointer;
+  }
+  @media (max-width: 1024px) {
+    width: 100%;
+    transition: width 0.3s;
+    .member-wrap {
+      flex-direction: column !important;
+    }
+    .item-wrap {
+      width: 100% !important;
+      justify-content: center !important;
+      flex-direction: row !important;
+    }
+  }
+  .item-border-right {
+    border-right: 1px solid #f7ebd5;
+  }
+  .divButtonStyle {
+    background-color: #f8ebd6;
+    color: #c5861f;
+  }
+`;
+const ReactCalendarListStyle = styled.div`
+  width: 100%;
+  border: 1px solid gray;
+  border-radius: 4px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  @media all and (max-width: 1024px) {
+    width: 100%;
+  }
+  ul {
+    display: flex;
+    width: 100%;
+    align-items: center;
+    //justify-content: space-around;
+  }
+  span {
+    display: block;
+    width: 100%;
+  }
+  li {
+    padding-left: 10px;
+    width: 100%;
+    border-bottom: 1px solid gray;
+    align-items: center;
+    display: flex;
+    font-size: 18px !important;
+    font-weight: 700;
+    color: black;
+    height: 40px;
+  }
+  .none-sch-div {
+    display: flex;
+    font-weight: bold;
+    margin: 0 auto;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid gray;
+    width: 100%;
+  }
+`;
+const CalendarListUlStyle = styled.div`
+  display: flex;
+  text-align: center;
+  a {
+    display: block;
+  }
+`;
+const CalendarListLiStyle = styled.div`
+  display: flex;
+  width: 100%;
+  li {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    justify-content: space-between;
+    height: 55px;
+  }
+  a {
+    width: 100%;
+  }
+  span {
+    display: block;
+    width: 100%;
+
+    text-align: center;
+  }
+`;
 let initailData = {};
 const MyMeetingSchDetail = () => {
   const [imgUrl, setImgUrl] = useState("meetinga.png");
@@ -125,6 +249,8 @@ const MyMeetingSchDetail = () => {
   const [isChangedVal, setIsChangedVal] = useState(false);
   const [isJoined, setIsJoined] = useState(false);
   const [memberSeq, setMemberSeq] = useState();
+  const [allData, setAllData] = useState([]);
+  const [isClicked, setIsClicked] = useState(0);
   const location = useLocation();
   const navigate = useNavigate();
   const param = useParams();
@@ -159,6 +285,7 @@ const MyMeetingSchDetail = () => {
     setIsCompleted(res.planCompleted);
     const memberSeq = await schMemberSeq();
     setMemberSeq(memberSeq);
+    getPlMemberSeq();
     console.log(location.state.isAuth);
   };
 
@@ -252,6 +379,17 @@ const MyMeetingSchDetail = () => {
     });
   };
 
+  useEffect(() => {}, []);
+
+  const getPlMemberSeq = async () => {
+    const res = await getSchMemberAll(param.meetingschid);
+    setAllData(res);
+    const a = Number(sessionStorage.getItem("userSeq"));
+    const b = res?.filter(item => {
+      return Number(item?.userSeq) === Number(a);
+    });
+    setPlanMemberSeq(b[0]?.plmemberSeq);
+  };
   const changedValue = () => {
     if (
       initailData.planTitle !== planTitle ||
@@ -306,21 +444,23 @@ const MyMeetingSchDetail = () => {
                       >
                         일정종료
                       </button>
-                      <button
-                        className={"etc-btn"}
-                        onClick={() => {
-                          navigate("/review/write", {
-                            state: {
-                              planSeq: location.state.planSeq,
-                              planMemberSeq: planMemberSeq,
-                              planTitle: planTitle,
-                              partyName: location.state.partyName,
-                            },
-                          });
-                        }}
-                      >
-                        리뷰 작성
-                      </button>
+                      {planMemberSeq ? (
+                        <button
+                          className={"etc-btn"}
+                          onClick={() => {
+                            navigate("/review/write", {
+                              state: {
+                                planSeq: location.state.planSeq,
+                                planMemberSeq: planMemberSeq,
+                                planTitle: planTitle,
+                                partyName: location.state.partyName,
+                              },
+                            });
+                          }}
+                        >
+                          리뷰 작성
+                        </button>
+                      ) : null}
                       <button
                         style={{ marginLeft: "5px" }}
                         type="button"
@@ -373,7 +513,7 @@ const MyMeetingSchDetail = () => {
                     marginTop: "10px",
                   }}
                 >
-                  {!isJoined ? (
+                  {planMemberSeq ? (
                     <>
                       <button
                         className="etc-btn"
@@ -384,21 +524,23 @@ const MyMeetingSchDetail = () => {
                       >
                         참가완료
                       </button>
-                      <button
-                        className={"etc-btn"}
-                        onClick={() => {
-                          navigate("/review/write", {
-                            state: {
-                              planSeq: location.state.planSeq,
-                              planMemberSeq: planMemberSeq,
-                              planTitle: planTitle,
-                              partyName: location.state.partyName,
-                            },
-                          });
-                        }}
-                      >
-                        리뷰 작성
-                      </button>
+                      {isCompleted === 2 ? (
+                        <button
+                          className={"etc-btn"}
+                          onClick={() => {
+                            navigate("/review/write", {
+                              state: {
+                                planSeq: location.state.planSeq,
+                                planMemberSeq: planMemberSeq,
+                                planTitle: planTitle,
+                                partyName: location.state.partyName,
+                              },
+                            });
+                          }}
+                        >
+                          리뷰 작성
+                        </button>
+                      ) : null}
                       <button
                         style={{ marginLeft: "5px" }}
                         type="button"
@@ -739,25 +881,68 @@ const MyMeetingSchDetail = () => {
                       </button>
                     </div>
                   ) : null}
-                  {/* <div className="flex-column">
-                    <label htmlFor="noticecontent">모임 일정 장소</label>
-                    <div
-                      style={{
-                        width: "100%",
-                        height: "400px",
-                        border: "1px solid black",
-                      }}
-                    >
-                      지도 API 들어올 자리
-                    </div>
-                  </div> */}
                 </div>
-                <MyMeetingSchMemberList
-                  meetingId={param.meetingschid}
-                  setPlanMemberSeq={setPlanMemberSeq}
-                  setIsJoined={setIsJoined}
-                  setPlanMemberJoinFunc={setPlanMemberJoinFunc}
-                ></MyMeetingSchMemberList>
+                <ReactCalendarStyle>
+                  <label>신청 멤버 관리</label>
+                  <div className="member-wrap">
+                    <ReactCalendarListStyle>
+                      <CalendarListUlStyle>
+                        <li>
+                          <span>순번</span>
+                          <span>멤버명</span>
+                        </li>
+                      </CalendarListUlStyle>
+                      <div className="func-main" style={{ width: "100%" }}>
+                        <div className="func-main-inner">
+                          {isClicked === 1 ? (
+                            allData.length > 0 ? (
+                              allData.map((item, index) => (
+                                <CalendarListLiStyle key={item.userSeq}>
+                                  {/* 컴포넌트로 뺄꺼임 일단 테스트 */}
+                                  <li>
+                                    <span>{index + 1}</span>
+                                    <span>{item?.userName}</span>
+                                    <input
+                                      value={item.userSeq}
+                                      readOnly
+                                    ></input>
+                                  </li>
+                                </CalendarListLiStyle>
+                              ))
+                            ) : (
+                              <div className="none-sch-div">
+                                일정 신청한 멤버가 없습니다.
+                              </div>
+                            )
+                          ) : allData.length > 0 ? (
+                            allData.map((item, index) => (
+                              <CalendarListLiStyle key={item.pk}>
+                                {/* 컴포넌트로 뺄꺼임 일단 테스트 */}
+                                <li>
+                                  <span>{index + 1}</span>
+                                  <span>{item.userName}</span>
+                                  <input
+                                    style={{
+                                      width: "0",
+                                      height: "0",
+                                      border: "none",
+                                    }}
+                                    value={item.userSeq}
+                                    readOnly
+                                  ></input>
+                                </li>
+                              </CalendarListLiStyle>
+                            ))
+                          ) : (
+                            <div className="none-sch-div" style={{}}>
+                              일정 신청한 멤버가 없습니다.
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                    </ReactCalendarListStyle>
+                  </div>
+                </ReactCalendarStyle>
               </form>
             </div>
           </div>
