e7e37e2b55cafc8d0ae32c887b3112c0b1aa7d29
Minkyung-Park <mp.minkyung@gmail.com>
2024-08-07 16:32:34 +0900

    feat : 마이페이지 작업중 (찜 목록 수정중)

--- src/pages/user/InfoEdit.js
+++ src/pages/user/InfoEdit.js
@@ -256,13 +256,10 @@ const InfoEdit = () => {
         alert("프로필 사진이 성공적으로 변경되었습니다.");
         // Redux 상태 업데이트
         dispatch(setUser({ userPic: data.resultData }));
-        // 미리보기 이미지 업데이트
-        setPreviewImage(data.resultData);
       } else {
         alert("프로필 사진 변경에 실패했습니다.");
       }
     } catch (error) {
-      console.error(error);
       alert(
         `프로필 사진 변경에 실패했습니다. 다시 시도해주세요. (${error.message})`,
       );
